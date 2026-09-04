87eb745747919655a46c425ec548844a01b62baf
andersbor <anbo@zealand.dk>
2026-09-04 19:59:56 +0200

    Add project files.

---
+++ RESTdogs/Models/Dog.cs
@@ -0,0 +1,14 @@
+﻿namespace RESTdogs.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"Dog: {Name}, Weight: {Weight}";
+        }
+    }
+}
---
+++ RESTdogs/Models/DogsRepositoryList.cs
@@ -0,0 +1,48 @@
+﻿namespace RESTdogs.Models
+{
+    public class DogsRepositoryList
+    {
+        private List<Dog> dogs = new List<Dog>();
+        private int nextId = 1;
+
+        public IEnumerable<Dog> GetDogs()
+        {
+            return dogs.ToList();
+        }
+
+        public Dog? GetDog(int id)
+        {
+            return dogs.FirstOrDefault(d => d.Id == id);
+        }
+
+        public Dog AddDog(Dog dog)
+        {
+            dog.Id = nextId++;
+            dogs.Add(dog);
+            return dog;
+        }
+
+        public bool UpdateDog(int id, Dog updatedDog)
+        {
+            var existingDog = GetDog(id);
+            if (existingDog == null)
+            {
+                return false;
+            }
+            existingDog.Name = updatedDog.Name;
+            existingDog.Weight = updatedDog.Weight;
+            return true;
+        }
+
+        public Dog? DeleteDog(int id)
+        {
+            var dog = GetDog(id);
+            if (dog == null)
+            {
+                return null;
+            }
+            dogs.Remove(dog);
+            return dog;
+        }
+    }
+}
